fb16dfe5279ce43e222d4521fb424c8df1fa45f0
Santiagossebastian <sebastianrodrigs099@gmail.com>
2024-12-03 13:49:50 -0300

    Perguntas ok

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 function criaCartao(categoria, pergunta, resposta){
-    let container= document. gatLementeById('container')
+    let container= document.getElementeById('container')
     let cartao=document.createElement('article')
     cartao.className='cartao'
 
-    cartao.innerHTML='
-    <dic class="cartao__conteudo">
+    cartao.innerHTML=`
+    <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
-        <p>${pergunta}</p>
+    <p>${pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
-        <p>${ressposta}></p>
+        <p>${resposta}</p>
     </div>
     </div>
-    '
+    `
 
     let respostaEstaVisivel = false
 
